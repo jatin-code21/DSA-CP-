3a197876517fa0a46b7a7081dda5c6f71acf04dc
Jatin <ozajatin9309@gmail.com>
2022-01-24 21:35:51 +0530

    postorder traversal recursive method

---
+++ Trees/postorder_traversal.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void postorder(TreeNode *root) //(Left Right Root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",
+            "r", stdin);
+    freopen("output.txt",
+            "w", stdout);
+#endif
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+    postorder(root);
+}
