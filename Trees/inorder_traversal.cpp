21fc421c4a498d7e0291fc41ad850b19cbb223ca
Jatin <ozajatin9309@gmail.com>
2022-01-24 21:06:28 +0530

    inorder traversal recusrsive method

---
+++ Trees/inorder_traversal.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void inorder(TreeNode *root) // (Left Root Right)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << (root->data) << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",
+            "r", stdin);
+    freopen("output.txt",
+            "w", stdout);
+#endif
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+    inorder(root);
+}
