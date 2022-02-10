a3eb6f1fccb4ac1b7a3fdffba06c99d78cf79104
Jatin <ozajatin9309@gmail.com>
2022-02-10 23:16:05 +0530

    Quick Sort Updated

--- Quick_sort.cpp
+++ Quick_sort.cpp
@@ -31,7 +31,7 @@ void quickSort(int arr[], int l, int r)
 {
     if (l < r)
     {
-        int pi = partition(arr, l, r);
+        int pi = partition(arr, l, r); // index where pivot element should be
         quickSort(arr, l, pi - 1);
         quickSort(arr, pi + 1, r);
     }
