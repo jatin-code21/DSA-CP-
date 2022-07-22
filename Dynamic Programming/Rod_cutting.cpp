0c246b133a6eeca42d6afe5e168d2209984d1878
Jatin Oza <ozajatin9309@gmail.com>
2022-07-23 00:25:38 +0530

    Rod cutting problem solved both approach dp

--- Dynamic Programming/Rod_cutting.cpp
+++ Dynamic Programming/Rod_cutting.cpp
@@ -1,6 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Recursive solution
+int max_profit(vector<int> &prices, int n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int cut = i + 1;
+        int cur_ans = prices[i] + max_profit(prices, n - cut);
+        ans = max(ans, cur_ans);
+    }
+
+    return ans;
+}
+
+// Bottom-up solution
+int max_profit_dp(vector<int> &prices, int n)
+{
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    for (int len = 1; len <= n; len++)
+    {
+        int ans = INT_MIN;
+        for (int i = 0; i < len; i++)
+        {
+            int cut = i + 1;
+            int cur_ans = dp[len - cut] + prices[i];
+            ans = max(ans, cur_ans);
+        }
+        dp[len] = ans;
+    }
+
+    return dp[n];
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -9,4 +48,10 @@ int main()
     freopen("output.txt",
             "w", stdout);
 #endif
+    int n;
+    cin >> n; // length of the rod intiially
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++)
+        cin >> prices[i];
+    cout << max_profit_dp(prices, n) << "\n";
 }
