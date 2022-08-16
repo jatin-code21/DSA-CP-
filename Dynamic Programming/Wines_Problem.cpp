6a83beb80a4b5ecbb85dc58a12d17d7e72135661
Jatin Oza <ozajatin9309@gmail.com>
2022-08-16 23:58:58 +0530

    upated

---
+++ Dynamic Programming/Wines_Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &p)
+{
+
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0;i < n; i++) cin >> p[i];
+}
