ad648f106d9ad7ff4dc3fd9a909a6572e0ea1df9
TheBeast <ozajatin9309@gmail.com>
2023-05-21 23:30:15 +0530

    Obst added

---
+++ Trees/OBST.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> dp(10001,vector<int>(10001,-1));
+// In this i am making an upper diagonal matrix
+// It can be made lower diagonal matrix also
+
+int sumFreq(vector<int> &freq, int i, int j)
+{
+    int sum = 0;
+    for (int k = i; k <= j; k++)
+    {
+        sum += freq[k];
+    }
+    return sum;
+}
+int cost(int i, int j, vector<int> &freq)
+{
+    if (i > j) return 0;
+    if (i == j) return dp[i][j] = freq[i];
+    if (dp[i][j] != -1) return dp[i][j];
+
+    int wt = sumFreq(freq, i, j);
+    int mn_cost = INT_MAX;
+    for (int k = i; k <= j; k++)
+    {
+        int temp = cost(i, k - 1, freq) + cost(k + 1, j, freq);
+        mn_cost = min(temp, mn_cost);
+    }
+    return dp[i][j] = mn_cost + wt;
+}
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> g(n+1);
+    for(int i=0;i<m;i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    vector<int> keys(n,0);
+    vector<int> freq(n,0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> keys[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> freq[i];
+    }
+    int ans = cost(0,n-1,freq);
+    cout << ans << "\n";
+}
